96fdcc9df5c4abac823c0d328ac61fc7bf6ced4b
Iris Kutsyy <ak2149@cam.ac.uk>
2020-03-01 20:06:37 +0000

    Made arm.py prettier

--- Control/arm.py
+++ Control/arm.py
@@ -42,6 +42,7 @@ class Arm:
     '''
         Initializing the arm initializes the connection with the EV3 and moves the arm to a neutral position
     '''
+
     def __init__(self, host=s.HOST, port=s.PORT):
         # Initialize server-client communications with EV3, using the default host and port if not specified
         self.server = s.Server(host, port)
@@ -55,6 +56,7 @@ class Arm:
     '''
     Moves a piece from (x1,y1) to (x2,y2), or from (x1,y1) to 'OFF', which means to drop the piece off the board
     '''
+
     def move_piece(self, instructions):
         (from_x, from_y), to_coord = instructions
         # Collect piece
@@ -69,19 +71,22 @@ class Arm:
     '''
         Move to off_board location and drop any held piece
     '''
+
     def drop_piece(self):
-        self.move_to(*OFF_POSITION,steps=2)
+        self.move_to(*OFF_POSITION, steps=2)
         self.retract_claw()
 
     '''
         Alias for easy use
     '''
+
     def move_out_of_way(self):
         self.drop_piece()
 
     '''
         Carefully move to above the piece, then slowly lower and pick up or deposit piece, then return to neutral
     '''
+
     def collect_or_drop(self, x, y, pick_up):
         if self.cart_pos != REST_POS:
             self.move_to(*REST_POS, steps=2)
@@ -95,6 +100,7 @@ class Arm:
         Move the claw, picking a piece up if pick_up is True
         and dropping the piece if pick_up is False
     '''
+
     def move_claw(self, pick_up):
         if pick_up:
             self.extend_claw()
@@ -105,6 +111,7 @@ class Arm:
         Moves to the position x,y,z, doing the move in the number of steps specified
         Blocks until the move is completed
     '''
+
     def move_to(self, x, y, z, steps=1):
         # Use the same move_to function for each of the steps
         if steps > 1:
@@ -112,9 +119,9 @@ class Arm:
             x_step = (x - self.cart_pos[0]) / steps
             y_step = (y - self.cart_pos[1]) / steps
             z_step = (z - self.cart_pos[2]) / steps
-            start_x, start_y,start_z = self.cart_pos
+            start_x, start_y, start_z = self.cart_pos
 
-            for i in range(1,steps+1):
+            for i in range(1, steps + 1):
                 # Move incrementally, linearly interpolating between the start and end positions
                 self.move_to(start_x + x_step * i, start_y + y_step * i, start_z + z_step * i, steps=1)
         # Single movement
@@ -135,9 +142,9 @@ class Arm:
             self.server.send_pos(MOTOR_ELBOW, int(elbow * GEAR_ELBOW))
 
             # Calculate amount of time to wait for EV3 to finish (send_pos is non-blocking)
-            theta_time = abs(self.angle_pos[0] - theta - 180)/SPEED_THETA
-            shoulder_time = abs(self.angle_pos[1] - shoulder)/SPEED_SHOULDER
-            elbow_time = abs(self.angle_pos[2] - elbow)/SPEED_ELBOW
+            theta_time = abs(self.angle_pos[0] - theta - 180) / SPEED_THETA
+            shoulder_time = abs(self.angle_pos[1] - shoulder) / SPEED_SHOULDER
+            elbow_time = abs(self.angle_pos[2] - elbow) / SPEED_ELBOW
             time.sleep(theta_time + shoulder_time + elbow_time + WAIT_CONST)
 
             # Update store of arm position
@@ -148,13 +155,13 @@ class Arm:
         if not self.claw_retracted:
             self.server.send_pos(MOTOR_CLAW, 0)
             # Waits for the same amount of time as to extend the claw
-            time.sleep(CLAW_EXTENDED_POS/SPEED_CLAW + CLAW_WAIT_CONST)
+            time.sleep(CLAW_EXTENDED_POS / SPEED_CLAW + CLAW_WAIT_CONST)
             self.claw_retracted = True
 
     def extend_claw(self):
         if self.claw_retracted:
             self.server.send_pos(MOTOR_CLAW, CLAW_EXTENDED_POS)
-            time.sleep(CLAW_EXTENDED_POS/SPEED_CLAW + CLAW_WAIT_CONST)
+            time.sleep(CLAW_EXTENDED_POS / SPEED_CLAW + CLAW_WAIT_CONST)
             self.claw_retracted = False
 
     # Magic positions that set the claw to the start state for easy reset
